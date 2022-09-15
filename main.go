78552235fe106904e2353ab51c6661df2341265c
novikovmu <novikovmu@elocont.ru>
2022-09-15 10:58:53 +0300

    MSP-1021 refactor main

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/integration-system/isp-kit/bootstrap"
+	"github.com/integration-system/isp-kit/shutdown"
+	"isp-script-service/assembly"
+	"isp-script-service/conf"
+	"isp-script-service/routes"
+)
+
 var version = "1.0.0"
 
 // @title isp-script-service
@@ -15,5 +23,26 @@ var version = "1.0.0"
 //go:generate rm -f docs/swagger.json
 
 func main() {
+	boot := bootstrap.New(version, conf.Remote{}, routes.EndpointDescriptors())
+	app := boot.App
+	logger := app.Logger()
 
+	assembly, err := assembly.New(boot)
+	if err != nil {
+		logger.Fatal(app.Context(), err)
+	}
+	app.AddRunners(assembly.Runners()...)
+	app.AddClosers(assembly.Closers()...)
+
+	shutdown.On(func() {
+		logger.Info(app.Context(), "starting shutdown")
+		app.Shutdown()
+		logger.Info(app.Context(), "shutdown completed")
+	})
+
+	err = app.Run()
+	if err != nil {
+		app.Shutdown()
+		logger.Fatal(app.Context(), err)
+	}
 }
