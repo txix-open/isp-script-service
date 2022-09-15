f62f6bb4e67371a0f33cba1cf8596fc8e15687cc
novikovmu <novikovmu@elocont.ru>
2022-09-15 10:56:55 +0300

    MSP-1021 refactor conf

---
+++ conf/remote.go
@@ -0,0 +1,28 @@
+package conf
+
+import (
+	"reflect"
+
+	"github.com/integration-system/isp-kit/log"
+	"github.com/integration-system/isp-kit/rc/schema"
+	"github.com/integration-system/jsonschema"
+)
+
+func init() {
+	schema.CustomGenerators.Register("logLevel", func(field reflect.StructField, t *jsonschema.Type) {
+		t.Type = "string"
+		t.Enum = []interface{}{"debug", "info", "error", "fatal"}
+	})
+}
+
+type Remote struct {
+	LogLevel                 log.Level `schemaGen:"logLevel" schema:"Уровень логирования"`
+	Scripts                  []ScriptDefinition
+	SharedScript             string
+	ScriptExecutionTimeoutMs int `valid:"required~Required"`
+}
+
+type ScriptDefinition struct {
+	Id     string `valid:"required~Required"`
+	Script string `valid:"required~Required"`
+}
